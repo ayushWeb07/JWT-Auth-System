cac3795579d9b6f4c81d3ea981f8492dbbab7403
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-01 01:38:08 +0530

    feat: Create script for running the server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import morgan from "morgan"
+import {bootstrapServer} from "./utils/server/bootstrap.server.ts";
 
 // create server instance
 const app= express()
@@ -7,3 +8,6 @@ const app= express()
 // setup global middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+
+// spin up the server + DB
+bootstrapServer(app)
