5dee247512f5bb3a27ff0e0031a3c267d550cd85
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-01 01:53:48 +0530

    feat: Create the health status endpoint

---
+++ src/routers/v1/health.router.ts
@@ -0,0 +1,8 @@
+import {Router} from "express";
+import * as healthController from "../../controllers/health.controller.ts"
+
+const router = Router();
+
+router.get("/", healthController.checkHealthStatus);
+
+export default router
