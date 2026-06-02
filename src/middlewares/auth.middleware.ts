0884584c1bce2b5ee37b7371d1df47ef4e933e2e
Ayush Bommana <bommanaayush07@gmail.com>
2026-06-02 09:13:56 +0530

    feat: Create the express type including the userId in request

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,38 @@
+import type { Request, Response, NextFunction } from "express";
+import {
+	ForbiddenError,
+	UnauthorizedError,
+} from "../utils/errors/app.error.ts";
+import jwt from "jsonwebtoken";
+import { serverConfig } from "../config/index.ts";
+
+const authHandler = (req: Request, res: Response, next: NextFunction) => {
+	try {
+		// get the token from headers
+		const authHeader = req.headers["authorization"];
+
+		if (!authHeader || !authHeader.startsWith("Bearer ")) {
+			throw new UnauthorizedError(
+				"Access denied: No or invalid token has been provided",
+			);
+		}
+
+		// verify the token
+		const token = authHeader.split(" ")[1];
+		const decoded = jwt.verify(token, serverConfig.JWT_SECRET_KEY);
+
+		// attach the user id to req. and then call the next middlewares
+		req.userId = decoded.userId;
+		next();
+	} catch (error) {
+		if (error instanceof UnauthorizedError) {
+			throw error;
+		} else {
+			throw new ForbiddenError(
+				"Access denied: Invalid or expired token has been provided",
+			);
+		}
+	}
+};
+
+export { authHandler };
